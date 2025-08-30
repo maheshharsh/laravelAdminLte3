c27e74385ca3a9b7df081c5d8f6673627416d96c
rupesh.soni <rupeshsoni1819@gmail.com>
2025-08-30 19:35:46 +0530

    Share article and og:image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <meta property="og:title" content="Samachar House News">
+    <meta property="og:description" content="Hindi news portal covering politics, climate, and local updates.">
+    <meta property="og:image" content="https://samacharhouse.com/images/logo.jpeg">
+    <meta property="og:url" content="https://samacharhouse.com/">
+    <meta property="og:type" content="website">
+
+    <meta name="title" content="Samachar House News">
+    <meta name="description" content="Hindi news portal covering politics, climate, and local updates related to bikaner.">
+    <link rel="image_src" href="https://samacharhouse.com/images/logo.jpeg">
+
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     @vite(['resources/js/app.tsx', 'resources/css/app.css'])
